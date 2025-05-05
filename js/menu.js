b6dc7e9d3d3a9fdf176928429848e6edb7ccbdba
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-05-05 07:52:57 +0200

    added comments

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
+/*
+ * This script creates an evenlistener for the burguer menu button
+ * which toggles the class "active" (becoming display: flex) for the 
+ * menu. Giving its functionality.
+ */
+
 document.addEventListener('DOMContentLoaded', function () {
     const burger = document.querySelectorAll('.bars-icon')[0];
     const menu = document.querySelectorAll('.collapse-menu-mobile')[0];
